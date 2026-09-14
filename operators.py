7cd761fbe3370ed07ef81c7f24a39db73119fa7f
BilalAhmed369 <chbilalahmed369@gmail.com>
2026-09-14 21:07:38 +0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,40 @@
+ # arithmatic operators
+
+print(6 + 3)
+print(6 - 3)
+print(6 * 3)
+print(6 / 3)
+print(6 // 3)
+print(6 % 3) #modulo -> reminder
+print(6 **3)
+
+
+#assignment operator
+x = 5
+# x = 5+1
+x += 3
+print(x)
+
+
+
+# operator precedence  ->BODMAS
+ans = 2 + 4 * 5
+ans1 = (2 + 4) * 5
+print(ans)
+print(ans1)
+
+
+
+#comparison operator ->  <,>,>=,<=, ==, !=
+print(3 > 2)
+print(1 > 4)
+print(1 <= 5)
+print(2 == 2)
+print(3 != 5)
+
+
+
+# logical operators -> or, and, not
+print((3 < 5) or (5 < 4))  # at least one statement should be true for true result
+print((5 > 2) and (9 < 3)) # both of the statements should be true 
+print(not (3 > 6))
