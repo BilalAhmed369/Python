c4bb531d0b071419f0fd073a1ddaf931a3384f42
BilalAhmed369 <chbilalahmed369@gmail.com>
2026-09-22 08:43:12 +0500

    Add files via upload

---
+++ input, output.py
@@ -0,0 +1,33 @@
+# taking output
+print("Bilal Ahmed")
+
+a = 15
+b = 20
+print(a+b)
+
+# checking variables
+
+a = "Hello"
+b = 123
+c = 3.5
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+
+ # taking input from the user
+name = input("Enter you name: ")
+print("Welcome", name)
+
+
+
+#                                             Exercise
+name = input('Enter your first name:')
+name1 = input('Enter your last name:')
+print('Hi', name, name1)
+print("Bilal's age is 21")
+print("Bilal's height is 1,85m")
